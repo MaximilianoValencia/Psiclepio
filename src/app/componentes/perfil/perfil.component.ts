4eb2b18f093ad4bf2308afc70c859198271fde84
MaximilianoValencia <maximiliano.valencia.s@mail.pucv.cl>
2023-06-11 17:03:29 -0400

    perfil

--- src/app/componentes/perfil/perfil.component.ts
+++ src/app/componentes/perfil/perfil.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent {
-
+  profilePhoto: string ="";
+  name: string ="";
+  dateOfBirth: string ="";
+  gender: string ="";
+  email: string ="";
+  phoneNumber: string="";
+  aboutMe: string="";
 }
