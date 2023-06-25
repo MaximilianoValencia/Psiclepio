30624fb52d1f123afb7c657f990adae530dfbfed
MaximilianoValencia <maximiliano.valencia.s@mail.pucv.cl>
2023-06-25 11:27:34 -0400

    perfil edit y guardar front

--- src/app/componentes/perfil/perfil.component.ts
+++ src/app/componentes/perfil/perfil.component.ts
@@ -6,12 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent {
+
   nombre: string = 'Nombre de la Persona';
   fechaNacimiento: string = 'DD/MM/YY';
   genero: string = 'Género de la persona';
   mail: string = 'mail@mail.com';
   numero: string = '+569 999 999 99';
-
   isPaciente: boolean = false;
   isProfesional: boolean = false;
+  descriptionText: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet ligula purus, et sodales purus tristique ac.';
+
+
+
+  isEditMode: boolean = false;
+
+  toggleEditMode() {
+    if(this.isEditMode){
+      this.guardarPerfil();
+    }else{
+      this.editPerfil();
+    }
+    this.isEditMode = !this.isEditMode;
+  }
+
+
+  editPerfil(){
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach((input: HTMLInputElement) => {
+      input.disabled = false;
+      input.style.backgroundColor = 'white';
+    });
+  }
+
+  guardarPerfil(){
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach((input: HTMLInputElement) => {
+      input.disabled = true;
+      input.style.backgroundColor = '#B3DDD3';
+    });
+
+    //POST REQ TODO
+
+    console.log("Inputs saved!");
+  }
+
 }
+
+
